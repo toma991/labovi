068584c357fe0103d7c02c95d577d5e9a810afe0
toma991 <147489515+toma991@users.noreply.github.com>
2023-10-18 12:34:04 +0200

    Update vjezba1.c
    risen prvi zad

--- vjezba1/vjezba1/vjezba1.c
+++ vjezba1/vjezba1/vjezba1.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX_LENGTH 50
 
-void main() {
-	printf("hello world\n");
-	
+typedef struct _student {
+    char ime[MAX_LENGTH];
+    char prezime[MAX_LENGTH];
+    int bodovi;
+} student;
+
+int main() {
+    int i = 0, count = 0;
+    char ch;
+    FILE* data;
+    data = fopen("studenti.txt", "r");
+    if (data == NULL) {
+        printf("Error: Unable to open the file.\n");
+        return 1; 
+    }
+
+    while ((ch = fgetc(data)) != EOF) {  
+        if (ch == '\n')
+            count++;
+    }
+
+    fclose(data);
+    printf("Number of lines in the file: %d\n", count);
+    return 0;
+}
 }
